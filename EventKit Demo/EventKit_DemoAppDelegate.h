56e76fb1149cef07ddbeb9a8b91467eb76e42e96
neilang <neilang@gmail.com>
2011-09-25 12:58:53 +1000

    Initial Commit

---
+++ EventKit Demo/EventKit_DemoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  EventKit_DemoAppDelegate.h
+//  EventKit Demo
+//
+//  Created by Neil Ang on 25/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventKit_DemoAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ EventKit Demo/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  EventKit Demo
+//
+//  Created by Neil Ang on 25/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
